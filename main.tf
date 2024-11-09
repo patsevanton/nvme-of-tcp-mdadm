9a92837848c490a345c9c7e1cccd1839539b6e0c
Anton Patsev <patsev.anton@gmail.com>
2024-11-09 17:12:32 +0600

    init

---
+++ main.tf
@@ -0,0 +1,55 @@
+data "yandex_client_config" "client" {}
+
+module "network" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-vpc.git?ref=v1.0.0"
+
+  folder_id = data.yandex_client_config.client.folder_id
+
+  blank_name = "instance-minimal-vpc-nat-gateway"
+  labels = {
+    repo = "terraform-yacloud-modules/terraform-yandex-vpc"
+  }
+
+  azs = ["ru-central1-a"]
+
+  private_subnets = [["10.1.10.0/24"]]
+
+  create_vpc         = true
+  create_nat_gateway = true
+}
+
+module "node1" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-instance.git?ref=v1.0.0"
+
+  folder_id = data.yandex_client_config.client.folder_id
+
+  name = "node1"
+
+  zone       = "ru-central1-a"
+  subnet_id  = module.network.private_subnets_ids[0]
+  enable_nat = true
+  create_pip = true
+
+  hostname         = "node1"
+  generate_ssh_key = false
+  ssh_user         = "ubuntu"
+  ssh_pubkey       = "~/.ssh/id_rsa.pub"
+}
+
+module "node2" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-instance.git?ref=v1.0.0"
+
+  folder_id = data.yandex_client_config.client.folder_id
+
+  name = "node2"
+
+  zone       = "ru-central1-a"
+  subnet_id  = module.network.private_subnets_ids[0]
+  enable_nat = true
+  create_pip = true
+
+  hostname         = "node2"
+  generate_ssh_key = false
+  ssh_user         = "ubuntu"
+  ssh_pubkey       = "~/.ssh/id_rsa.pub"
+}
---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "instance_private_ip_node1" {
+  value = module.node1.instance_private_ip
+}
+
+output "instance_public_ip_node1" {
+  value = module.node1.instance_public_ip
+}
+
+output "instance_private_ip_node2" {
+  value = module.node2.instance_private_ip
+}
+
+output "instance_public_ip_node2" {
+  value = module.node2.instance_public_ip
+}
